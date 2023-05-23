688cccbde51aa4a84532f0de909868cc4e88117f
Themir <98977837+themirforget@users.noreply.github.com>
2023-05-23 17:20:00 -0300

    Loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
-import { SignOutButton } from "@clerk/clerk-react";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
 
 dayjs.extend(relativeTime);
 
@@ -59,13 +59,25 @@ const PostView = (props: PostWithUser) => {
   );
 };
 
+const Feed = () => {
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+  if (!postsLoading) return <LoadingPage />;
+  if (!data) return <div>Something went wrong</div>;
+  return (
+    <div className="flex flex-col">
+      {[...data, ...data]?.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}{" "}
+    </div>
+  );
+};
+
 const Home: NextPage = () => {
-  const user = useUser();
+  const {isLoaded: userLoaded, isSignedIn } = useUser();
 
-  const { data, isLoading } = api.posts.getAll.useQuery();
+  api.posts.getAll.useQuery();
 
-  if (!data || isLoading) return <div>Loading...</div>;
-  if (!data) return <div>Something is wrong</div>;
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -78,20 +90,15 @@ const Home: NextPage = () => {
         <div className="h-full w-full border-x border-slate-400 md:max-w-2xl ">
           <div className="flex border-b border-slate-400 p-4">
             {" "}
-            {!user.isSignedIn && (
+            {!isSignedIn && (
               <div className="flex justify-center">
                 {" "}
                 <SignInButton />{" "}
               </div>
             )}{" "}
-            {user.isSignedIn && <CreatePostWizard />}
+            {isSignedIn && <CreatePostWizard />}
           </div>
-
-          <div className="flex flex-col">
-            {[...data, ...data]?.map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}{" "}
-          </div>
+          <Feed />
         </div>
       </main>
     </>
